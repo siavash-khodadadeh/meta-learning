b6dac716db402eb16f375db758ac8f8d2e070a2c
Siavash Khodadadeh <siavash.khodadadeh@gmail.com>
2018-06-04 18:35:47 -0400

    check split bug.

--- models.py
+++ models.py
@@ -343,11 +343,11 @@ class ModelAgnosticMetaLearning(object):
         input_validation_labels_splits = tf.split(self.input_validation_labels, len(self.devices))
 
         input_data_splits = [
-            self.input_data[0, :, :, :, :].reshape(1, -1, -1, -1, -1),
-            self.input_data[1, :, :, :, :].reshape(1, -1, -1, -1, -1),
-            self.input_data[2, :, :, :, :].reshape(1, -1, -1, -1, -1),
-            self.input_data[3, :, :, :, :].reshape(1, -1, -1, -1, -1),
-            self.input_data[4, :, :, :, :].reshape(1, -1, -1, -1, -1),
+            tf.reshape(self.input_data[0, :, :, :, :], (1, -1, -1, -1, -1)),
+            tf.reshape(self.input_data[1, :, :, :, :], (1, -1, -1, -1, -1)),
+            tf.reshape(self.input_data[2, :, :, :, :], (1, -1, -1, -1, -1)),
+            tf.reshape(self.input_data[3, :, :, :, :], (1, -1, -1, -1, -1)),
+            tf.reshape(self.input_data[4, :, :, :, :], (1, -1, -1, -1, -1)),
         ]
 
         optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
