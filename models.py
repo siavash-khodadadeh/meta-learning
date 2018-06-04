0c3e019e200b9f96e1929c76d16b8f8551c564c0
Siavash Khodadadeh <siavash.khodadadeh@gmail.com>
2018-06-04 11:34:39 -0400

    Print the model variables in order to debug the problem

--- models.py
+++ models.py
@@ -353,6 +353,7 @@ class ModelAgnosticMetaLearning(object):
                         model_out_train = model.output
                         self.inner_model_out.append(model_out_train)
                         self.model_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='model')
+                        print(self.model_variables)
 
                     with tf.variable_scope('loss', reuse=tf.AUTO_REUSE):
                         if learn_the_loss_function:
@@ -396,11 +397,11 @@ class ModelAgnosticMetaLearning(object):
         with tf.variable_scope('meta_optimizer'):
             self.meta_loss = tf.add_n(self.tower_losses)
             meta_optimizer = tf.train.AdamOptimizer(learning_rate=self.meta_learn_rate)
-            # self.gradients = meta_optimizer.compute_gradients(self.meta_loss, colocate_gradients_with_ops=True)
-            #
-            # for grad_info in self.gradients:
-            #     if grad_info[0] is not None:
-            #         tf.summary.histogram(grad_info[1].name, grad_info[0])
+            self.gradients = meta_optimizer.compute_gradients(self.meta_loss, colocate_gradients_with_ops=True)
+
+            for grad_info in self.gradients:
+                if grad_info[0] is not None:
+                    tf.summary.histogram(grad_info[1].name, grad_info[0])
 
             self.train_op = meta_optimizer.minimize(self.meta_loss, colocate_gradients_with_ops=True)
 
