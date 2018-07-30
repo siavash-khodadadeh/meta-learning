9b658c4ea3bf05decd4d0d44a0dc68f762d1997c
Siavash Khodadadeh <siavash.khodadadeh@gmail.com>
2018-07-30 14:56:45 -0400

    Run training on ucf 101.

--- executions/meta_learning_execution.py
+++ executions/meta_learning_execution.py
@@ -12,52 +12,31 @@ import settings
 
 
 META_TRAIN = False  # true if we want to do meta train otherwise performing meta-test.
-DATASET = 'diva'  # from 'kinetics', 'ucf-101', 'omniglot' or 'diva'.
-N = 5  # Train an N-way classifier.
+DATASET = 'ucf-101'  # from 'kinetics', 'ucf-101', 'omniglot' or 'diva'.
+N = 27  # Train an N-way classifier.
 K = 1  # Train a K-shot learner
 
 NUM_ITERATIONS = 1000
 REPORT_AFTER_STEP = 20
 SAVE_AFTER_STEP = 500
-BATCH_SIZE = 5  # The batch size.
+BATCH_SIZE = 9  # The batch size.
 META_LEARNING_RATE = 0.0001
 LEARNING_RATE = 0.001
 
 NUM_META_TEST_ITERATIONS = 101
 SAVE_AFTER_META_TEST_STEP = 30
 
-NUM_GPUS = 1  # Number of GPUs to use for training.
+NUM_GPUS = 3  # Number of GPUs to use for training.
 RANDOM_SEED = 100  # Random seed value. Set it to -1 in order not to use a random seed.
 STARTING_POINT_MODEL_ADDRESS = os.path.join(settings.PROJECT_ADDRESS, 'MAML/sports1m_pretrained.model')
 
 
-META_TEST_MODEL = '/backups/kinetics-from-server/-9900'
-META_TEST_STARTING_MODEL = settings.SAVED_MODELS_ADDRESS + META_TEST_MODEL
+META_TEST_MODEL = 'kinetics/meta-train/27-way-classifier/1-shot/batch-size-9/num-gpus-3/random-seed-100/' \
+                  'num-iterations-1000/meta-learning-rate-0.0001/learning-rate-0.001/-1000'
+META_TEST_STARTING_MODEL = os.path.join(settings.SAVED_MODELS_ADDRESS + META_TEST_MODEL)
 
 
-test_actions = [
-    'CleanAndJerk',
-    'MoppingFloor',
-    'FrontCrawl',
-    'Surfing',
-    'Bowling',
-    'SoccerPenalty',
-    'SumoWrestling',
-    'Shotput',
-    'PlayingSitar',
-    'FloorGymnastics',
-    'Typing',
-    'JumpingJack',
-    'ShavingBeard',
-    'FrisbeeCatch',
-    'WritingOnBoard',
-    'JavelinThrow',
-    'Fencing',
-    'FieldHockeyPenalty',
-    'BaseballPitch',
-    'CuttingInKitchen',
-    'Kayaking',
-]
+test_actions = os.listdir('/home/siavash/ucf101_tfrecords/')
 
 
 def initialize():
