c6cd6d302c3e02dc9b1c082f7dfb41b13dd49ccf
Siavash Khodadadeh <siavash.khodadadeh@gmail.com>
2018-08-01 19:39:30 -0400

    Train based on sigmoid for multi class meta learning.

--- executions/meta_learning_execution.py
+++ executions/meta_learning_execution.py
@@ -11,8 +11,8 @@ from models import ModelAgnosticMetaLearning, C3DNetwork
 import settings
 
 
-META_TRAIN = False  # true if we want to do meta train otherwise performing meta-test.
-DATASET = 'diva'  # from 'kinetics', 'ucf-101', 'omniglot' or 'diva'.
+META_TRAIN = True  # true if we want to do meta train otherwise performing meta-test.
+DATASET = 'kinetics'  # from 'kinetics', 'ucf-101', 'omniglot' or 'diva'.
 N = 20  # Train an N-way classifier.
 K = 1  # Train a K-shot learner
 
@@ -52,6 +52,7 @@ def initialize():
     model_dir = os.path.join(
         DATASET,
         'meta-train',
+        'sigmoid-loss-function',
         '{}-way-classifier'.format(N),
         '{}-shot'.format(K),
         'batch-size-{}'.format(BATCH_SIZE),
