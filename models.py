Fix a bug in sending average grads for averaging the gradients on meta loss.

models.py
@@ -403,9 +403,9 @@ class ModelAgnosticMetaLearning(object):
                     self.tower_meta_grads.append(meta_grads)
 
                     if averaged_meta_grads is None:
-                        avereged_meta_grads = average_gradients([meta_loss, meta_loss])
+                        avereged_meta_grads = average_gradients([meta_grads, meta_grads])
                     else:
-                        averaged_meta_grads = average_gradients([avereged_meta_grads, meta_loss])
+                        averaged_meta_grads = average_gradients([avereged_meta_grads, meta_grads])
 
         # with tf.variable_scope('average_gradients'):
             # with tf.device('/cpu:0'):